56df5bf005b31926a11b1ae0b0faf4991e80ef23
Davide Gerbaudo <davide.gerbaudo@gmail.com>
2012-03-27 16:52:42 +0300

    drop TriggerEff (not needed)

--- do.sh
+++ do.sh
@@ -1,11 +1,9 @@
-
    mkdir MyRootCoreDir
    cd MyRootCoreDir
    source ~/script/bash/setup_atlas.sh 
    asetup AtlasP1HLT,17.1.4.5,here
 
    svn co svn+ssh://svn.cern.ch/reps/atlasoff/PhysicsAnalysis/D3PDTools/RootCore/tags/RootCore-00-01-00 RootCore
-   svn co svn+ssh://svn.cern.ch/reps/atlasgrp/Institutes/LIP/RootCore/TriggerEff/trunk TriggerEff
    svn co svn+ssh://svn.cern.ch/reps/atlasoff/DataQuality/GoodRunsLists/tags/GoodRunsLists-00-00-94 GoodRunsLists
    svn co svn+ssh://svn.cern.ch/reps/atlasoff/Trigger/TrigAnalysis/TrigRootAnalysis/tags/TrigRootAnalysis-00-00-08  TrigRootAnalysis
    cd RootCore/
