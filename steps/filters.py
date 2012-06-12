8398847582e425a33454c153445524e6ab44a187
gerbaudo <davide.gerbaudo@gmail.com>
2012-06-12 14:42:04 +0200

    use any() instead of len(filter())

--- steps/filters.py
+++ steps/filters.py
@@ -1,7 +1,7 @@
 from supy import analysisStep
 
 class triggers(analysisStep) :
-    def select(self,ev) : return len(filter(lambda t: ev[t] == True, self.triggers))
+    def select(self,ev) : return any(ev[t]==True for t in self.triggers)
     def __init__(self, triggers = []) :
         self.triggers = triggers
         self.moreName = '_'.join([t for t in self.triggers])
