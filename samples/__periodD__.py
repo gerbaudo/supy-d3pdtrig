66ce553c95d06116a4e7aa91f06f4e1ea08e4b51
gerbaudo <davide.gerbaudo@gmail.com>
2012-10-04 17:23:27 +0200

    allow per-run spec

--- samples/__periodD__.py
+++ samples/__periodD__.py
@@ -1,3 +1,4 @@
+
 from supy.samples import SampleHolder
 from supy.sites import eos
 
@@ -8,17 +9,11 @@ lumiPerRun = {208184:105.2, 208258:92.55, 208261:103.1, 208354:133.2, # D4, D5
               208485:142.2, 208662:149.0, # D6, D7
               }
 
-def skimmedPeriodD(skim='') :
+def skimmedPeriodD(skim='', run=None) :
     assert skim in skims,"periodD: this skim is not available :%s"%skim
     periodD = SampleHolder()
-    fileListCmd = '+'.join(['%s%s")'%(eos(),"%s/SUSYD3PD.%d.skim.%s"%(eosBaseDir,run,skim)) for run in lumiPerRun.keys()])
-    periodD.add("periodD.%s"%skim, fileListCmd, lumi = sum(lumiPerRun.values()))
+    if run : assert run in lumiPerRun,"periodD: invalid run %d for this period"%run
+    runs = [run] if run else lumiPerRun.keys()
+    fileListCmd = '+'.join(['%s%s")'%(eos(),"%s/SUSYD3PD.%d.skim.%s"%(eosBaseDir,run,skim)) for run in runs])
+    periodD.add("periodD.%s"%skim, fileListCmd, lumi = sum([lumiPerRun[run] for run in runs]))
     return periodD
-
-# might want to define per-run samples?
-##for r in runs :
-##    for d in ['EF_A4_OR_A10','L1_RD0_FILLED'] :
-##        exampleDict.add("%d.%s"%(r,d),
-##                        '%s%s")'%(supy.sites.eos(),
-##                                  "%s/SUSYD3PD.%d.skim.%s"%(eosBaseDir,r,d)),
-##                        lumi=lumiPerRun[r])
