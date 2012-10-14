fae2ee316368fd7e3fafe3f46ae28caf1272c1a8
gerbaudo <davide.gerbaudo@gmail.com>
2012-10-14 19:05:17 +0200

    siwtch to using samples functions; cleanup

--- analyses/display_L2FSPS.py
+++ analyses/display_L2FSPS.py
@@ -18,14 +18,10 @@ class display_L2FSPS(supy.analysis) :
         pars = self.parameters()
         outList=[
             supy.steps.printer.progressPrinter(),
-            #steps.filters.triggers(["EF_5j55_a4tchad_L2FS"]),
-            #steps.filters.triggers(["EF_5j55_a4tchad_L2FSPS"]).invert(),
-            #steps.filters.triggers(["L2_5j15_a4TTem"]),
-            #steps.filters.triggers(["L2_5j15_a4TTem_5j50_a4cchad"]).invert(),
-            #steps.filters.triggers(['EF_6j50_a4tchad_L2FS_5L2j15']),
-            steps.filters.triggers(['EmulatedL2FSPS_6j50']),
-            steps.filters.triggers(['L2_6j15_a4TTem_6j50_a4cchad']).invert(),
-            
+            steps.filters.triggers(['EmulatedOffline_6j70']),
+            steps.filters.triggers(['EF_6j50_a4tchad_L2FS_5L2j15']),
+            steps.filters.triggers(['EF_6j50_a4tchad_L2FSPS_5L2j15']).invert(),
+
             steps.displayer.displayer(doL1Jets=True, doL2Jets=True, doEfJets = True, doOfflineJets=True),
             ]
         return outList
@@ -43,7 +39,9 @@ class display_L2FSPS(supy.analysis) :
                               calculables.TrigD3PD.TriggerBit("L2_5j15_a4TTem"),
                               calculables.TrigD3PD.TriggerBit("L2_5j15_a4TTem_5j50_a4cchad"),
                               calculables.TrigD3PD.TriggerBit("L2_6j15_a4TTem_6j50_a4cchad"),
-                              calculables.TrigD3PD.TriggerBit("EF_6j50_a4tchad_L2FS_5L2j15"),
+                              #calculables.TrigD3PD.TriggerBit("EF_6j50_a4tchad_L2FS_5L2j15"),
+                              calculables.TrigD3PD.TriggerBit('EF_6j50_a4tchad_L2FS_5L2j15'),
+                              calculables.TrigD3PD.TriggerBit('EF_6j50_a4tchad_L2FSPS_5L2j15'),
                               ]
         listOfCalculables += [calculables.jet.IndicesL1(collection=("trig_L1_jet_", "")),
                               calculables.jet.L1Jets(),
@@ -75,72 +73,21 @@ class display_L2FSPS(supy.analysis) :
                                                                               label='L2FSPS',
                                                                               multi=6, minEt=55.*GeV),
                               ]
+        emjb = calculables.TrigD3PD.EmulatedMultijetTriggerBit
+        listOfCalculables += [emjb(jetColl='OfflineJets', label='Offline', multi=6, minEt=70.*GeV)]
 
         return listOfCalculables
 
     def listOfSampleDictionaries(self) :
-        exampleDict = supy.samples.SampleHolder()
-        # get these files from
-        # eos ls /eos/atlas/atlasdatadisk/data11_7TeV/NTUP_TRIG/r3408_r3410_p661/data11_7TeV.00191628.physics_EnhancedBias.merge.NTUP_TRIG.r3408_r3410_p661_tid742401_00
-        # (these are the suspicious events from Brian's email)
-        #exampleDict.add("Pythia_ttbar_bWincbHminus",
-        #                'utils.fileListFromDisk(location = "/tmp/gerbaudo/eos/NTUP*.root*", isDirectory = False)',
-        #                lumi = 1.0e+3 ) #/pb
-        # this file is from Bertrand's D3PD
-        # dq2-ls -f user.chapleau.valid1.105204.TTbar_FullHad_McAtNlo_Jimmy.recon.AOD.e825_s1310_s1300_r3391.NTUP_TRIG.JetOnly.v1/
-#        exampleDict.add("Pythia_ttbar_bWincbHminus",
-#                        'utils.fileListFromDisk(location = "/tmp/gerbaudo/dq2/user.chapleau.001130.EXT0._00082.NTUP.root", isDirectory = False)',
-#                        lumi = 1.0e+3 ) #/pb
-#        exampleDict.add("ttbar-00-01-28",
-#                        '%s%s")'%(supy.sites.eos(), "/eos/atlas/user/g/gerbaudo/trigger/bugfixCheck/TrigT2CaloJet-00-01-28"),
-#                        lumi = 1.0e3)
-#        exampleDict.add("ttbar-00-01-29",
-#                        '%s%s")'%(supy.sites.eos(), "/eos/atlas/user/g/gerbaudo/trigger/bugfixCheck/TrigT2CaloJet-00-01-29"),
-#                        lumi = 1.0e3)
-#        exampleDict.add("ttbar-00-01-31",
-#                        '%s%s")'%(supy.sites.eos(), "/eos/atlas/user/g/gerbaudo/trigger/bugfixCheck/TrigT2CaloJet-00-01-31"),
-#                        lumi = 1.0e3)
-#        exampleDict.add("data12_8TeV.00202609",
-#                        'utils.fileListFromTextFile(fileName="//afs/cern.ch/work/g/gerbaudo/public/trigger/MyRootCoreDir/supy-d3pdtrig/data/data12_8TeV.00202609_f.txt")',
-#                        lumi = 0.313)
-#        exampleDict.add("data12_8TeV.00202660",
-#                        'utils.fileListFromTextFile(fileName="//afs/cern.ch/work/g/gerbaudo/public/trigger/MyRootCoreDir/supy-d3pdtrig/data/data12_8TeV.00202660.txt")',
-#                        lumi = 2.178)
-#        exampleDict.add("data12_8TeV.00202668",
-#                        'utils.fileListFromTextFile(fileName="//afs/cern.ch/work/g/gerbaudo/public/trigger/MyRootCoreDir/supy-d3pdtrig/data/data12_8TeV.00202668.txt")',
-#                        lumi = 26.11)
-#        exampleDict.add("data12_8TeV.00202798",
-#                        'utils.fileListFromTextFile(fileName="//afs/cern.ch/work/g/gerbaudo/public/trigger/MyRootCoreDir/supy-d3pdtrig/data/data12_8TeV.00202798.txt")',
-#                        lumi = 26.11)
-
-        exampleDict.add("PeriodB_L1_4J15",
-                        'utils.fileListFromTextFile('
-                        +'fileName="/afs/cern.ch/work/g/gerbaudo/public/trigger/MyRootCoreDir/supy-d3pdtrig/data/periodB.txt"'
-                        +')',
-                        lumi=26.11+29.85+7.281+52.6)
-
-        return [exampleDict]
+        skim = 'L1_4J15'
+        return [samples.skimmedPeriodD(skim=skim, run=208258)]
 
     def listOfSamples(self,config) :
-        return (#supy.samples.specify(names = "Pythia_ttbar_bWincbHminus", color = r.kBlack, markerStyle = 20,
-                #                     #nFilesMax = 100,
-                #                     #nEventsMax=10,
-                #                     )
-#                supy.samples.specify(names = "ttbar-00-01-29", color = r.kBlack, markerStyle = 20,
-#                                     nFilesMax = 1,
-#                                     nEventsMax=1000,
-#                                     )
-#                supy.samples.specify(names = "ttbar-00-01-31", color = r.kBlack, markerStyle = 20,
-#                                     nFilesMax = 1,
-#                                     nEventsMax=1000,
-#                                     )
-                #supy.samples.specify(names = "data12_8TeV.00202609",   color = r.kBlack, nEventsMax=-1, nFilesMax=-1,)
-                #supy.samples.specify(names = "data12_8TeV.00202660",   color = r.kBlack, nEventsMax=10000, nFilesMax=-1,)
-                supy.samples.specify(names = "PeriodB_L1_4J15",   color = r.kBlack, nEventsMax=1000, nFilesMax=-1,)
-                #supy.samples.specify(names = "data12_8TeV.00202668",   color = r.kBlack, nEventsMax=1000, nFilesMax=1,)
-                #supy.samples.specify(names = "data12_8TeV.00202798",   color = r.kBlack, nEventsMax=1000, nFilesMax=-1,)
-
-                )
+        test = False #True
+        nEventsMax= 100000 if test else None
+        nFilesMax=100 if test else None
+        skim = 'L1_4J15'
+        return ([] + supy.samples.specify(names="periodD.%s"%skim, nEventsMax=nEventsMax, nFilesMax=nFilesMax))
 
     def conclude(self,pars) :
         #make a pdf file with plots from the histograms created above
@@ -148,6 +95,4 @@ class display_L2FSPS(supy.analysis) :
         org.scale(lumiToUseInAbsenceOfData=1000.)
         supy.plotter( org,
                       pdfFileName = self.pdfFileName(org.tag),
-                      #samplesForRatios = ("Example_Skimmed_900_GeV_Data","Example_Skimmed_900_GeV_MC"),
-                      #sampleLabelsForRatios = ("data","sim"),
                       ).plotAll()
