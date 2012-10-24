2f8cb9cf06cda5cae7ba059888b8f44f08fea348
gerbaudo <davide.gerbaudo@gmail.com>
2012-10-24 20:02:55 +0200

    add generic jet printer

--- steps/printer.py
+++ steps/printer.py
@@ -1,5 +1,17 @@
 from supy import utils,analysisStep
 
+class JetPrinter(analysisStep) :
+    def __init__(self, collection='') :
+        self.collection = collection
+        self.leavesToPrint=['E','eta','phi',]
+
+    def uponAcceptance (self,eventVars) :
+        jets = eventVars[self.collection]
+        print self.collection
+        print '\n'.join(["[%d] : %s" % (i," ".join(["%s : %s"%(a, getattr(j,a))
+                                                    for a in self.leavesToPrint]
+                                                   + ["et : %.1f"%j.et]))
+                         for i,j in enumerate(jets)])
 class l2JetPrinter(analysisStep) :
     def __init__(self, collection='') :
         self.collection = collection
@@ -10,7 +22,7 @@ class l2JetPrinter(analysisStep) :
                             'InputType',
                             'OutputType',
                             ]
-        
+
     def uponAcceptance (self,eventVars) :
         jets = eventVars[self.collection]
         print self.collection
