04caccd753bfb68ac3160283a9d5380e611f8c76
gerbaudo <davide.gerbaudo@gmail.com>
2012-10-24 18:55:43 +0200

    jet spectrum histo and turn-on with list comprehension

--- analyses/fatJetTurnOn.py
+++ analyses/fatJetTurnOn.py
@@ -25,6 +25,7 @@ class fatJetTurnOn(supy.analysis) :
                 'turnOnTrigger' : toTrigs[mode],
                 'skim' : skims[mode],
                 'refJetColl' : 'OfflineJets',
+                'refFatJetColl' : 'OfflineJetsA10',
                 'offlineFatJetColl':'jet_AntiKt10LCTopo_',
                 'efCalibTag':'AntiKt4_lctopo',
                 }
@@ -36,6 +37,7 @@ class fatJetTurnOn(supy.analysis) :
         tonTrigger = pars['turnOnTrigger']
         refJetColl = pars['refJetColl']
         offlineFatJetColl = pars['offlineFatJetColl']
+        refFatJetColl = pars['refFatJetColl']
         xMin, xMax = 0. , (200. if mode=='j35' else 600.)
         binWidth = 5 if mode=='j35' else 10.
         nBins = int((xMax-xMin)/binWidth)
@@ -49,14 +51,20 @@ class fatJetTurnOn(supy.analysis) :
             steps.filters.goodRun().onlyData(),
             steps.filters.triggers([refTrigger]),
             #supy.steps.printer.printstuff(['PassedTriggers',]),
-            steps.histos.attribute(attrName='et', coll=refJetColl, nTh=0, title="E_{T} %dth jet "%(0+1)+"("+refJetColl+"); E_{T}; events",xLo=xMin,xUp=xMax*GeV),
             ]
-        outList += [
-            steps.histos.turnOnJet(trigger=tonTrigger, jetColl=refJetColl, nTh=0,N=nBins,low=xMin,up=xMax,emulated=emulated,
-                                   title=tonTrigger+" efficiency; 1st AntiKt4TopoNewEM jet E_{T} [GeV];eff"),
-            steps.histos.turnOnJet(trigger=tonTrigger, jetColl='OfflineJetsA10', nTh=0,N=nBins,low=xMin,up=xMax,emulated=emulated,
-                                   title=tonTrigger+" efficiency; 1st AntiKt10LCTopo jet E_{T} [GeV];eff"),
-            ]
+
+        outList += [steps.histos.attribute(attrName='et', coll=jc, nTh=0,
+                                           title="E_{T} %dth jet "%(0+1)+"("+jc+"); E_{T}; events",
+                                           xLo=xMin,xUp=xMax*GeV)
+                    for jc in [refJetColl,refFatJetColl]
+                    ]
+        outList += [steps.histos.turnOnJet(trigger=tonTrigger, jetColl=jc,
+                                            nTh=0,N=nBins,low=xMin,up=xMax,
+                                            emulated=emulated,
+                                            title=tonTrigger+" efficiency; 1st %s jet E_{T} [GeV];eff"%jcl)
+                     for jc, jcl in zip([refJetColl, refFatJetColl],
+                                        ['AntiKt4TopoNewEM', 'AntiKt10LCTopo'])
+                     ]
         return outList
 
     def listOfCalculables(self,config) :
@@ -80,7 +88,6 @@ class fatJetTurnOn(supy.analysis) :
         listOfCalculables += [calculables.jet.IndicesEf(minEt=minEt, maxEta=maxEta, calibTag=calibTag),
                               calculables.jet.EfJets(indices='IndicesEfJets'+calibTag),
                               ]
-
         listOfCalculables += [calculables.jet.IndicesOffline(minEt=minEt, maxEta=maxEta),
                               calculables.jet.OfflineJets(),
                               calculables.jet.IndicesOfflineBad(),
