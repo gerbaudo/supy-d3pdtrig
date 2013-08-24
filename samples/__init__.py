6342afe625b3db686fdde8ddb3c9976d7972a279
gerbaudo <davide.gerbaudo@gmail.com>
2013-08-24 18:52:11 +0200

    marco samples

--- samples/__init__.py
+++ samples/__init__.py
@@ -4,6 +4,7 @@
 #    'periodD',
 #    ] : exec("from __%s__ import %s"%(module, module))
 
+from __marcoSamples__ import getMarcoSamples
 from __periodD__ import skimmedPeriodD
 from __periodB__ import skimmedPeriodB
 from __rd0__ import minbiasPeriodD
